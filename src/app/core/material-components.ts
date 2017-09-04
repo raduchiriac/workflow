cb75bfb12bf79cb3ecfbe67ed0ed3aaef1715717
CHIRIAC Radu <radu.chiriac@ext.ec.europa.eu>
2017-09-04 14:32:38 +0200

    Adding Material Design

--- src/app/core/material-components.ts
+++ src/app/core/material-components.ts
@@ -1,8 +1,7 @@
-import { MdButtonModule, MdCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MdButtonModule, MdCheckboxModule],
-  exports: [MdButtonModule, MdCheckboxModule],
+  imports: [],
+  exports: [],
 })
 export class MaterialComponents { }
