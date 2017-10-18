3d5a7a205906a8b0cc263f1bf69637fc492c9161
Radu Chiriac <contact@raduchiriac.me>
2017-10-18 23:34:20 +0200

    Add Various Types

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ module.exports = (db) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers", "Authorization, Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, x-xsrf-token");
     next();
   });
 
